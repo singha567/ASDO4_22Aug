56afcbfe507645827e6b5ed6ff85c17f310a7700
Arti Singh <arti.singh@saga.co.uk>
2023-09-11 14:38:10 +0100

    if else work

--- Python/Lab1/Lab1/Lab1.py
+++ Python/Lab1/Lab1/Lab1.py
@@ -1,1 +1,6 @@
 
+print('Hello World!')
+
+username='Bob'
+age=32
+print(username,'is',age,'years old')
---
+++ Python/selection_ex.py
@@ -0,0 +1,19 @@
+# selection example
+
+inputvar = int(input('Read a number, not 7'))
+if inputvar == 7:
+    print("You typed 7!!")
+    result = inputvar^2
+    print("Result = ",result)
+else:
+    print("well done")
+
+# mple sequential test using elif
+if inputvar < 10:
+    print("single digit value")
+elif inputvar <100:
+    print("a double digit")
+elif inputvar < 1000:
+    print("a triple digit")
+else:
+    print("huge")
