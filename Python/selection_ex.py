ededa190f2711145ffdbbac7bddb275df1c8eed9
Arti Singh <arti.singh@saga.co.uk>
2023-09-11 14:49:24 +0100

    practise for if else

--- Python/selection_ex.py
+++ Python/selection_ex.py
@@ -9,6 +9,7 @@ else:
     print("well done")
 
 # mple sequential test using elif
+#make sure to have things in correct order
 if inputvar < 10:
     print("single digit value")
 elif inputvar <100:
@@ -17,3 +18,7 @@ elif inputvar < 1000:
     print("a triple digit")
 else:
     print("huge")
+
+#nesting if statements
+# good to have or compare different variables
+
