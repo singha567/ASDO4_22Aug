d92669ad84d69f8d92816aadb93f6267ea71771c
Arti Singh <arti.singh@saga.co.uk>
2023-09-12 18:16:35 +0100

    Lab work

--- Python/Lab1/Lab2/TimeCalculator.py
+++ Python/Lab1/Lab2/TimeCalculator.py
@@ -1,7 +1,7 @@
 # Task 2 - time Calculator
-vardt1 = str(input('Date 1 in format DD:MM:YY => '))
-vardt2 = str(input('Date 2 in format DD:MM:YY => '))
-print(vardt1,'  ',vardt2)
+#vardt1 = str(input('Date 1 in format DD:MM:YY => '))
+#vardt2 = str(input('Date 2 in format DD:MM:YY => '))
+#print(vardt1,'  ',vardt2)
 
 print('Time Calculator')
 print('1- Add 2 times ')
@@ -14,6 +14,36 @@ print('7- Convert Days to Time ')
 print('8- Exit')
 print('')
 
-varoption = int(input('          Enter an option => '))
-print(varoption)
+var_option = 1
+dt3 = []
+while var_option in range(1,9):
+    var_option = int(input('          Enter an option => '))
+    print(var_option)
+    if var_option in (1,2):
+        vardt1 = input('Enter Date 1 in format DD:HH:MM => ')
+        dt1 = vardt1.split(':')
+        vardt2 = input('Enter Date 2 in format DD:HH:MM => ')
+        dt2 = vardt2.split(':')
+        print(vardt1,'  ',vardt2)
+        print(dt1)
+        print(dt2)
+        if var_option == 1:
+            dt3 = [dt1[0]+dt2[0], dt1[1]+dt2[1], dt1[2]+dt2[2]]
+            print(dt3)
+#            for i in range(0,3):
+#                dt3[i] = int(dt1[i]) + int(dt2[i])
+#            print(dt3)
+    elif var_option in (3,4,5):
+        vardt1 = str(input('Enter Date in format DD:HH:MM => '))
+        dt1 = vardt1.split(':')
+        if var_option == 3:
+            disp_val = (int(dt1[0])*24)+int(dt1[1])+int(int(dt1[2])/24)
+            print(disp_val)
+        elif var_option == 4:
+            disp_val = (int(dt1[0])*1440 + int(dt1[1]*60) + int(dt1[2]))
+            print(disp_val)
+    
 
+
+
+print('Program ended. Thanks! ')
