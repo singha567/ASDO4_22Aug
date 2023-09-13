a7fd917c105e6f09fca746a69ef230adc0b66a2f
Arti Singh <arti.singh@saga.co.uk>
2023-09-13 14:27:56 +0100

    correct error

--- Python/Lab1/Lab_Day3/tax.py
+++ Python/Lab1/Lab_Day3/tax.py
@@ -3,9 +3,11 @@
 
 def getIncometax(inputvar):
     pers_allowance = 11850
-    if inputvar < 34501:
+    if inputvar < pers_allowance:
+        taxcalc = 0
+    elif inputvar > 11851 and inputvar < 34501:
         taxcalc = (inputvar - pers_allowance)*0.20
-    elif inputvar < 1500001:
+    elif inputvar > 34500 and inputvar < 1500001:
         taxcalc = (inputvar - pers_allowance)*0.40
     else:
         taxcalc = (inputvar - pers_allowance)*0.45
@@ -13,10 +15,14 @@ def getIncometax(inputvar):
 
 #main
 print('Welcome')
-salary = float(input('Please enter your salary => '))
+salary = input('Please enter your salary => ')
 print(salary)
+if salary.isnumeric():
+    salary = float(salary)
+else:
+    salary = input('Please enter your salary in numeric value => ')
 
-print('Tax calculation on your salary = ',getIncometax(salary))
+print('Tax calculation on your salary = ',getIncometax(float(salary)))
 
 
 
