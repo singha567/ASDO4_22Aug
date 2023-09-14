5922c3eebfe6a59709db578c93fc13ae35da81b9
Arti Singh <arti.singh@saga.co.uk>
2023-09-14 09:58:49 +0100

    changes

--- Python/Lab1/Lab_Day3/carData.py
+++ Python/Lab1/Lab_Day3/carData.py
@@ -2,39 +2,36 @@
 
 fileobjectvar = open("Python/car.csv","r")
 
-comp_idx = 0
-sales_idx = 0
+grand_total = 0
+yr_sales = 0
 
 companies=[]
 sales=[]
+carsale=[]
 
 for x in range(0,5):
     line = fileobjectvar.readline()
-    print(line)
     data = line.strip().split(',')
-    print(data)
+#    print(data)
     companies.append(data[0])
-    #print(companies)
 
     print(len(data))
+    yr_sales = 0
     for i in range(1,len(data)):
         sales.append(data[i])
+        yr_sales = yr_sales + int(data[i])
 
-print('************')
+    carsale.append(yr_sales)
+    grand_total += yr_sales
+print('************ Companies ******')
 print(companies)
-print(sales)
+
+
 sales = list(map(int,sales))
 print(sales)
-
-
-        
-#   
-#    companies[comp_idx] = 
-
-#print(type(stringvar1))
-
-
-
-#for x in range(0,len(stringvar1),2):
-#    print(x)
-    #if x.isnumeric():
+print('******* Company\'s yearly sale  ******')
+for i in carsale:
+    print(i)
+#print(carsale)
+print('grand total of sales =>  ',grand_total)
+ 
