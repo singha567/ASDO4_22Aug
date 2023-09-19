cc504dd9d45238d74c08662a57b27fc26a094d36
Arti Singh <arti.singh@saga.co.uk>
2023-09-19 09:33:37 +0100

    cosmetic changes

--- Pyapp/test_lab_unittest.py
+++ Pyapp/test_lab_unittest.py
@@ -6,3 +6,16 @@ def test_count():
     assert lab_unittest.count((2,3,4,3),3) == 2
     assert lab_unittest.count(["a","bcd","a","ef"],"a") == 2
     assert lab_unittest.count((-2,-3,-4,-3),3) == 0
+
+def test_count_zeros():
+	assert lab_unittest.count([0,0,0],0)==3
+
+def test_count_string():
+	assert lab_unittest.count(["a","a","a"],"a")==3
+
+def test_count_minus():
+	assert lab_unittest.count([-1,-3,-5,-4], -1)==1
+
+def test_count_normalNum():
+	assert lab_unittest.count([1,2,3,4,5,6,6,5,4,3,2,1], 1)==2
+
